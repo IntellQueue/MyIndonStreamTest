bb27c53b324285d4bd559cc2a96813645aa634db
unknown <IntellQueue>
2026-05-23 13:48:18 +0700

    Fix DutaMovie domain and compilation issues

--- Dutamovie/src/main/kotlin/com/dutamovie/DutaMovie.kt
+++ Dutamovie/src/main/kotlin/com/dutamovie/DutaMovie.kt
@@ -16,7 +16,7 @@ import org.jsoup.nodes.Element
 
 class DutaMovie : MainAPI() {
 
-    override var mainUrl = "https://tuggycomputer.com/"
+    override var mainUrl = "https://itoshii-movie.com/"
     private var directUrl: String? = null
     override var name = "DutaMovie"
     override val hasMainPage = true
